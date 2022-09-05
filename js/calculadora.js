daeb26d18fb350b2963c8aec3833f422c262a459
MartiPech <christianmartipech@gmail.com>
2022-09-05 18:28:41 -0300

    tarea de eventos

--- js/calculadora.js
+++ js/calculadora.js
@@ -5,7 +5,7 @@ function limpiar(){
 function sumar(){
     var x = parseInt( document.getElementById('valor1').value);
     var y = parseInt( document.getElementById('valor2').value);
-    document.getElementById('el-resultado').innerHTML = x+y;
+    document.getElementById('el-resultado').innerHTML = (x+y);
 }
 
 function restar(){
@@ -25,3 +25,34 @@ function dividir(){
     var y = parseInt( document.getElementById('valor2').value);
     document.getElementById('el-resultado').innerHTML = (x/y);
 }
+
+let btn_add = document.getElementById('agregar');
+
+
+
+
+
+btn_add.addEventListener("click" , function(){
+
+
+    let resultado__ = document.getElementById('el-resultado');
+    let lista__ = document.getElementById("lista");
+
+
+    let li = document.createElement("li");
+
+    li.innerHTML = `<span>${resultado__.value}</span>`;
+
+    lista.append(li);
+
+
+    
+
+
+});
+
+
+
+
+
+
